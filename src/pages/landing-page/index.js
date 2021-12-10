6bc573a18b159e470cddfb1700d8d3aec23ba8e2
Vukasin993 <macura4vukasin@gmail.com>
2021-12-10 01:28:33 +0100

    Added new api for 7 days, new layout for mobile

--- src/pages/landing-page/index.js
+++ src/pages/landing-page/index.js
@@ -1,32 +1,66 @@
-import React, { useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import './landing-page.scss';
 import Input from '../../components/input/index';
-import Select from 'react-select'
 import search from '../../assets/images/search.svg';
+import cloudy from '../../assets/images/cloudy.svg';
+import ReactFlagsSelect from 'react-flags-select';
 
 const API_KEY = '4e308dad5b0ffc36440e738859db44c6';
 
 const LandingPage = () => {
 const [searchTerm, setSearchTerm] = useState('');
-const [weatherData, setWeatherData] = useState([]);
-// const [city, setCity] = useState('');
+const [weatherData, setWeatherData] = useState({});
+const [sevenDaysData, setSevenDaysData] = useState({});
 const [cityName, setCityName] = useState('');
 const [stateCode, setStateCode] = useState('');
-const [cnt, setCnt] = useState(10);
-// const API_KEY = '4e308dad5b0ffc36440e738859db44c6';
+const cnt = 10;
+const [update, setUpdate] = useState(false);
+const [updateWeather, setUpdateWeather] = useState(false);
+const [selected, setSelected] = useState('');
+const getMonth = { month: 'long'};
+const date = new Date();
 
+const isEmpty = (obj) => {
+  if (obj === null ||
+      obj === undefined ||
+      Array.isArray(obj) ||
+      typeof obj !== 'object'
+  ) {
+      return true;
+  }
+  return Object.getOwnPropertyNames(obj).length === 0;
+};
 
-const getWeather = () => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode}&cnt=${cnt}&appid=${API_KEY}`).then(
-            response => response.json()
-        ).then(
-            data => {
-                setWeatherData(data);
-            }
-        )
+const getWeather = async () => {
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode},${stateCode}&cnt=${cnt}&units=metric&appid=${API_KEY}`);
+        const response = await api_call.json();
+
+        if (stateCode !== response.sys.country) {
+          alert("CITY ISNT IN THIS COUNTRY")
+        } else {
+          setWeatherData(response);
+          setUpdateWeather(true);
+        }
+
 }
 
-console.log("WEATHER DATA++", weatherData)
+const sevendDaysWeather = async () => {
+  const api_call = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely&units=metric&appid=${API_KEY}`)
+  const response = await api_call.json();
+  setSevenDaysData(response);
+}
+
+console.log("weatherData ", weatherData)
+
+console.log("++++ ", sevenDaysData)
+
+
+
+useEffect(() => {
+  if (updateWeather) {
+    sevendDaysWeather();
+  }
+},[updateWeather]);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -34,46 +68,67 @@ console.log("WEATHER DATA++", weatherData)
     setCityName(e.target.value);
   };
 
-  const options = [
-    { value: 'NL', label: 'NL' },
-    { value: 'US', label: 'US' },
-    { value: 'GB', label: 'GB' }
-  ]
-
-  const [selectedOption, setSelectedOption] = useState(options[0].value);
-
-
+useEffect(() => {
+  if (stateCode !== '' && cityName !== '') {
+    getWeather();
+}
+}, [update])
   const getResult = () => {
     if (stateCode !== '' && cityName !== '') {
-        getWeather();
+       setUpdate(!update)
     }
   }
 
-  const handleChange = (e) => {
-      console.log("E",e)
-    setStateCode(e.value);
+  const handleCode = (code) => {
+    setStateCode(code);
+    setSelected(code)
   }
-
-  console.log("STATEA CODE", stateCode)
 
   return (
     <div className="wrapper">
-      <div className="search">
-        <div className="left">
-        <div className="icon">
-            <img src={search} alt="Search" />
+      <div className={!isEmpty(weatherData) ? "search" : "center-search"}>
+          <div className="icon">
+          {!isEmpty(weatherData) ? <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt={weatherData.weather[0].main}/> : <img src={cloudy} alt="cloudy" />}
           </div>
           <div className="select">
-            <Select options={options} value={options.find(el => el.value === selectedOption)} onChange={(e) => handleChange(e)} />
+            <ReactFlagsSelect
+              countries={["US", "GB", "FR","DE","IT", "NL", "RS", "HR"]}
+              customLabels={{"US": "US","GB": "GB","FR": "FR","DE": "DE","IT": "IT", "NL": "NL", "RS": "RS", "HR": "HR"}}
+              selected={selected}
+              onSelect={code => handleCode(code)}
+            />
           </div>
-        </div>
         <div className="right">
           <div className="input"><Input placeholder="Search" value={searchTerm} onChange={handleSearch} /></div>
           <div className="icon">
             <img src={search} alt="Search" onClick={getResult} />
           </div>
       </div>
-      </div>
+      </div>      
+      {!isEmpty(weatherData) ? <div>
+      <div className='ten-days-temp'>
+         <p className='date-range'>{new Intl.DateTimeFormat('en-US', getMonth).format(date)} {date.getDate()} - {date.getDate() + 7} {date.getFullYear()}</p>
+         <span className='average-temperature'>{Math.round(weatherData.main.temp)}<span className='celsius'>&#8451;</span></span>
+        </div>
+       
+        <div className="seven-days">
+        {!isEmpty(sevenDaysData) && sevenDaysData.daily.map((d,i) => {
+          if (i + 1 !== sevenDaysData.daily.length) {
+            return (
+              <div className="seven-days-card">
+                <img
+                  src={`http://openweathermap.org/img/w/${d.weather[0].icon}.png`}
+                  alt={d.weather[0].main}
+                />
+                <span className='card-day'>{new Date(d.dt*1000).toLocaleString('en-us', {weekday:'long'})}</span>
+                <span className='card-temperature'>{Math.round(d.temp.day)} &#8451;</span>
+              </div>        
+            )
+          }
+        })}
+        </div>
+      </div> : null }
+      {/* {/* <h2>{weatherData.main.temp}</h2> */}
     </div>
   );
 }
